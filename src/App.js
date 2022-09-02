0ba5de4dc5f666920f5f79d015b522e7a7445b29
suryawanshipriyankasystango <psuryawanshi@systango.com>
2022-09-02 14:42:41 +0530

    working on display the user list

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import AddUser from './components/Users/AddUsers';
+import UserList from './components/Users/UserList.js';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
